7dad3645a1c8b85a4b18f50d990f0834575565d8
udoyen <datameshprojects@gmail.com>
2021-10-21 13:10:32 +0100

    first commit

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -1,31 +1,33 @@
-import express, { Router, Request, Response } from 'express';
-import bodyParser from 'body-parser';
+// tslint:disable-next-line: ordered-imports
+import bodyParser from "body-parser";
+import express, { Request, Response, Router } from "express";
 
-import { Car, cars as cars_list } from './cars';
+import { Car, cars as cars_list } from "./cars";
 
 (async () => {
-  let cars:Car[]  = cars_list;
+  const cars: Car[]  = cars_list;
 
-  //Create an express application
-  const app = express(); 
-  //default port to listen
-  const port = 8082; 
-  
-  //use middleware so post bodies 
-  //are accessable as req.body.{{variable}}
-  app.use(bodyParser.json()); 
+  // Create an express application
+  const app = express();
+  // default port to listen
+  const port = 8082;
+
+  // use middleware so post bodies
+  // are accessable as req.body.{{variable}}
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
   // Root URI call
   app.get( "/", ( req: Request, res: Response ) => {
     res.status(200).send("Welcome to the Cloud!");
   } );
 
-  // Get a greeting to a specific person 
+  // Get a greeting to a specific person
   // to demonstrate routing parameters
   // > try it {{host}}/persons/:the_name
-  app.get( "/persons/:name", 
+  app.get( "/persons/:name",
     ( req: Request, res: Response ) => {
-      let { name } = req.params;
+      const { name } = req.params;
 
       if ( !name ) {
         return res.status(400)
@@ -39,7 +41,7 @@ import { Car, cars as cars_list } from './cars';
   // Get a greeting to a specific person to demonstrate req.query
   // > try it {{host}}/persons?name=the_name
   app.get( "/persons/", ( req: Request, res: Response ) => {
-    let { name } = req.query;
+    const { name } = req.query;
 
     if ( !name ) {
       return res.status(400)
@@ -52,9 +54,9 @@ import { Car, cars as cars_list } from './cars';
 
   // Post a greeting to a specific person
   // to demonstrate req.body
-  // > try it by posting {"name": "the_name" } as 
+  // > try it by posting {"name": "the_name" } as
   // an application/json body to {{host}}/persons
-  app.post( "/persons", 
+  app.post( "/persons",
     async ( req: Request, res: Response ) => {
 
       const { name } = req.body;
@@ -71,16 +73,59 @@ import { Car, cars as cars_list } from './cars';
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
 
+  // Get a list of cars
+  app.get("/cars/", (req: Request, res: Response) => {
+    const { make } = req.query;
+
+    let cars_list = cars;
+
+    // if we have an optional query paramater, filter by it
+    if (make) {
+      cars_list = cars.filter((car) => car.make === make);
+    }
+
+    // return the resulting list along with 200 success
+    res.status(200).send(cars_list);
+  });
+
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
+  app.get("/cars/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const car = cars.find((car) => {
+      return car.id === parseInt(id, 10);
+    });
+    if (!car) {
+      return res.status(404).send("Car not found");
+    }
+    return res.status(200).send(car);
+  });
 
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+  app.post("/cars", async (req: Request, res: Response) => {
+    const { make, type, model, cost } = req.body;
+
+    if (!make || !type || !model || !cost) {
+      return res.status(400).send("Make, type, model, and cost are required");
+    }
+    const car: Car = {
+      make,
+      type,
+      model,
+      cost,
+      id: cars.length + 1,
+    };
+    cars.push(car);
+    return res.status(201).send(car);
+  });
 
   // Start the Server
   app.listen( port, () => {
+      // tslint:disable-next-line: no-console
       console.log( `server running http://localhost:${ port }` );
+      // tslint:disable-next-line: no-console
       console.log( `press CTRL+C to stop server` );
   } );
 })();
